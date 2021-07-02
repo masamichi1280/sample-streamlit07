61dca168312c734b438f839e0b7de7410b3e27f9
masamichi1280 <masamichi1280@icloud.com>
2021-07-02 17:40:28 +0900

    1st commit

---
+++ .ipynb_checkpoints/gazo-checkpoint.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from PIL import Image
+
+st.title('顔認識アプリ')
+
+uploaded_file = st.file_uploader("Choose an image...", type='jpg')
+if uploaded_file is not None:
+    img = Image.open(uploaded_file)
+    st.image(img,caption='Uploaded Image.' ,use_column_width=True)
+
